d9b3c3ed8b8e3a388d2d850f40d2048455f015ea
capelaum <thecapellett@gmail.com>
2023-08-04 11:51:45 -0300

    feat: book sheet store

---
+++ src/stores/bookSheetStore.ts
@@ -0,0 +1,43 @@
+import { api } from '@/lib/api'
+import { Book } from '@/types/app'
+import { useQuery } from '@tanstack/react-query'
+import { create } from 'zustand'
+
+interface BookSheetStore {
+  book: Book | null
+  setBook: (book: Book) => void
+  isBookSheetOpen: boolean
+  setIsBookSheetOpen: (isBookSheetOpen: boolean) => void
+  toggleIsBookSheetOpen: (isBookSheetOpen: boolean) => void
+}
+
+export const bookSheetStore = create<BookSheetStore>((set) => ({
+  book: null,
+  setBook: (book) => set({ book }),
+  isBookSheetOpen: false,
+  setIsBookSheetOpen: (isBookSheetOpen) => set({ isBookSheetOpen }),
+  toggleIsBookSheetOpen: (isBookSheetOpen) =>
+    set({ isBookSheetOpen: !isBookSheetOpen })
+}))
+
+const getBook = async (id: string) => {
+  const response = await api(`/api/books/${id}`)
+
+  if (response.status !== 200) {
+    throw new Error('Error fetching questions')
+  }
+
+  const { data } = response
+
+  return data
+}
+
+export const useFetchBook = (id: string) => {
+  const { setBook } = bookSheetStore()
+
+  return useQuery(['book-sheet', id], () => getBook(id), {
+    onSuccess: (book) => {
+      setBook(book)
+    }
+  })
+}
